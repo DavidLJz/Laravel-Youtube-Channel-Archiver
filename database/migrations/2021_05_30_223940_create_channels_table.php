38c71698d8aeb2b4f1574877b2077a3ba2e34178
David LJ <4dlj1995@gmail.com>
2021-05-30 22:54:22 -0500

    Created channel model

---
+++ database/migrations/2021_05_30_223940_create_channels_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateChannelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('channels', function (Blueprint $table) {
+            $table->bigIncrements('pk');
+
+            $table->string('yt_id');
+            $table->string('name');
+            $table->integer('video_count');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('channels');
+    }
+}
